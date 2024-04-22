f3aafb6316b9c9be9323b8d5eabc0ac48ec917ac
Alex <alex.beciana@gmail.com>
2024-04-22 17:37:37 -0400

    Added third-party images config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                host: 'res.cloudinary.com',
+                port: '',
+                pathname: '/dymmbugh2/**'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
